f1e446ff3632e77e891f36ecc7069914bfa9649d
Jerry Le <69036036+lookoutmanjava@users.noreply.github.com>
2025-06-09 20:11:33 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
+USER_NAME = "lee1595"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "lee.leee1595.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMDUzMWFlYWM5ZjlkMjdmODM5MDI3MGZmMWMzMjY2M2UiLCJ0IjoiNjM1MzhlMGQtMTdlMC00ZTYyLThlZTctNjVlMWEwNGU5ZGMzIiwicyI6IlpqWTVaak00TW1NdE16azJZUzAwT0RVeExUaGlOMlF0TldZeU9EY3pNV1ptTkdJMSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
